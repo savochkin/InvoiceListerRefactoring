30118cc2ea84a4cd85c0545ae6b8f49b8c6b9c90
savochkin <57403553+savochkin@users.noreply.github.com>
2023-06-30 19:51:10 +0200

    Revert "Move IsContractedByBrazil to Debtor"

--- src/main/java/refactoring/domain/fin/Debtor.java
+++ src/main/java/refactoring/domain/fin/Debtor.java
@@ -3,15 +3,10 @@ package refactoring.domain.fin;
 import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
-import refactoring.FinConstants;
 
 @Data
 @Builder
 public class Debtor {
     final private Long debtorId;
     final private String contractedBy;
-
-    public boolean isContractedByBrazil() {
-        return FinConstants.CONTRACTED_BY_BRAZIL.equals(getContractedBy());
-    }
 }
