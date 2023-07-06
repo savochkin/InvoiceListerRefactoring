2ac8b933fcbe8ec753c501114aec712b53258ac5
Egor Savochkin <egor.savochkin@booking.com>
2023-07-06 15:02:28 +0200

    Fixed and refactored tests, bugfixes related to adyen and company

--- src/test/java/refactoring/service/invoices/InvoiceListerServiceTest.java
+++ src/test/java/refactoring/service/invoices/InvoiceListerServiceTest.java
@@ -1,30 +1,18 @@
 package refactoring.service.invoices;
 
-import refactoring.billing.models.SimpleInvoiceProjectionDTO;
+import refactoring.FinConstants;
 import refactoring.controller.ListInvoicesController;
-import refactoring.domain.fin.Debtor;
-import refactoring.domain.fin.FinanceInvoice;
-import refactoring.domain.fin.FinanceInvoiceBrazil;
 import refactoring.dto.InvoiceData;
 import org.junit.jupiter.api.Test;
-import refactoring.repository.fin.DebtorRepository;
-import refactoring.repository.fin.FinanceInvoiceBrazilRepository;
-import refactoring.service.billingengine.BillingEngineClient;
-import refactoring.service.invoices.brazil.BrazilInvoiceService;
-import refactoring.service.repo.fin.DebtorService;
-import refactoring.service.repo.fin.FinanceInvoiceBrazilService;
-import refactoring.service.repo.fin.FinanceInvoiceService;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
 
 class InvoiceListerServiceTest {
-    final private static long NON_BRAZIL_DEBTOR_ID = 111L;
-    final private static long BRAZIL_DEBTOR_ID = 222L;
+
 /*
    functional requirements:
    + for all invoices return basic information including:
@@ -41,211 +29,131 @@ class InvoiceListerServiceTest {
 
     @Test
     void getInvoicesFromNonBrazil() {
-        List<InvoiceData> invoices = getListInvoicesController().getInvoicesList(NON_BRAZIL_DEBTOR_ID, false);
+        List<InvoiceData> invoices = getListInvoicesController().getInvoicesList(DebtorFactory.getNonBrazilDebtorId(), false);
         assertNotNull(invoices);
-        assertEquals(getTestInvoices().size(), invoices.size());
+        assertEquals(getExpectedNonBrazilInvoices().size(), invoices.size());
         for(int i=0; i<invoices.size(); i++) {
-            assertEquals(getTestInvoices().get(i), invoices.get(i));
+            assertEquals(getExpectedNonBrazilInvoices().get(i), invoices.get(i));
         }
     }
 
     @Test
     void getInvoicesFromBrazil() {
-        List<InvoiceData> invoices = getListInvoicesController().getInvoicesList(BRAZIL_DEBTOR_ID, false);
+        List<InvoiceData> invoices = getListInvoicesController().getInvoicesList(DebtorFactory.getBrazilDebtorId(), false);
         assertNotNull(invoices);
-        assertEquals(getTestBrazilInvoices().size(), invoices.size());
-        for(InvoiceData invoice: getTestBrazilInvoices()) {
+        assertEquals(getExpectedBrazilInvoices().size(), invoices.size());
+        for(InvoiceData invoice: getExpectedBrazilInvoices()) {
             assertTrue(invoices.stream().anyMatch(i -> i.equals(invoice)));
         }
     }
 
     @Test
     void adyenAllowedInAllCountriesButBrazil() {
-        List<InvoiceData> invoices = getListInvoicesController().getInvoicesList(BRAZIL_DEBTOR_ID, false);
+        List<InvoiceData> invoices = getListInvoicesController().getInvoicesList(DebtorFactory.getBrazilDebtorId(), false);
         assertNotNull(invoices);
         for(InvoiceData invoice: invoices) {
             assertFalse(invoice.isAdyenAllowed());
         }
 
-        List<InvoiceData> invoices2 = getListInvoicesController().getInvoicesList(NON_BRAZIL_DEBTOR_ID, false);
+        List<InvoiceData> invoices2 = getListInvoicesController().getInvoicesList(DebtorFactory.getNonBrazilDebtorId(), false);
         assertNotNull(invoices2);
         for(InvoiceData invoice: invoices2) {
             assertTrue(invoice.isAdyenAllowed());
         }
     }
 
-
-    private ListInvoicesController getListInvoicesController() {
-        DebtorService debtorService = getDebtorService();
-        BrazilInvoiceService brazilInvoiceService = getBrazilInvoiceService();
-        FinanceInvoiceService financeInvoiceService = getFinanceInvoiceService();
-        BillingEngineClient billingEngineClient = getBillingEngineClient();
-        InvoiceListerService invoiceListerService = new InvoiceListerService(debtorService, financeInvoiceService, billingEngineClient, brazilInvoiceService);
+    public static ListInvoicesController getListInvoicesController() {
+        InvoiceListerService invoiceListerService = new InvoiceListerService(
+                DebtorFactory.getDebtorService(), CoreBillingFactory.getFinanceInvoiceService(),
+                NbeFactory.getBillingEngineClient(), CoreBillingFactory.getBrazilInvoiceService());
         return new ListInvoicesController(invoiceListerService);
     }
 
-    private BillingEngineClient getBillingEngineClient() {
-        BillingEngineClient billingEngineClient = mock(BillingEngineClient.class);
-        when(billingEngineClient.getInvoices(NON_BRAZIL_DEBTOR_ID)).thenReturn(getNbeInvoices());
-        when(billingEngineClient.getInvoices(BRAZIL_DEBTOR_ID)).thenReturn(getNbeBrazilInvoices());
-        return billingEngineClient;
-    }
-
-    private FinanceInvoiceService getFinanceInvoiceService() {
-        FinanceInvoiceService financeInvoiceService = mock(FinanceInvoiceService.class);
-        when(financeInvoiceService.getInvoicesForHotel(NON_BRAZIL_DEBTOR_ID)).thenReturn(getTestFinanceInvoices());
-        when(financeInvoiceService.getInvoicesForHotel(BRAZIL_DEBTOR_ID)).thenReturn(getTestFinanceBrazilInvoices());
-        return financeInvoiceService;
-    }
-
-    private DebtorService getDebtorService() {
-        DebtorRepository debtorRepository = mock(DebtorRepository.class);
-        when(debtorRepository.getDebtorById(NON_BRAZIL_DEBTOR_ID)).thenReturn(getTestDebtor());
-        when(debtorRepository.getDebtorById(BRAZIL_DEBTOR_ID)).thenReturn(getBrazilDebtor());
-        return new DebtorService(debtorRepository);
-    }
-
-    private BrazilInvoiceService getBrazilInvoiceService() {
-        FinanceInvoiceBrazilRepository financeInvoiceBrazilRepository = mock(FinanceInvoiceBrazilRepository.class);
-        when(financeInvoiceBrazilRepository.getByInvoiceIds()).thenReturn(getFinanceInvoiceBrazilInvoices());
-
-        FinanceInvoiceBrazilService financeInvoiceBrazilService = new FinanceInvoiceBrazilService(financeInvoiceBrazilRepository);
-        BrazilInvoiceService brazilInvoiceService = new BrazilInvoiceService(financeInvoiceBrazilService);
-        return brazilInvoiceService;
-    }
-
-    private List<FinanceInvoiceBrazil> getFinanceInvoiceBrazilInvoices() {
-        FinanceInvoiceBrazil brazil1 = FinanceInvoiceBrazil.builder()
-                .invoiceId(20220203L)
-                .rpsNumber(8888L)
-                .prefeituraUrl("http://www.prefeitura.com/20220203")
-                .build();
-        return List.of(brazil1);
-    }
-
-    private List<SimpleInvoiceProjectionDTO> getNbeInvoices() {
-        SimpleInvoiceProjectionDTO invoice1 = SimpleInvoiceProjectionDTO.builder()
-                .externalId("20230303")
-                .invoiceDate(LocalDate.of(2023, 3, 3))
-                .assetId(111L)
-                .settlementInvoiceAmount(BigDecimal.valueOf(233.03))
-                .paidState("paid")
-                .invoiceType("reservation_statement")
-                .build();
-        SimpleInvoiceProjectionDTO invoice2 = SimpleInvoiceProjectionDTO.builder()
-                .externalId("20230403")
-                .invoiceDate(LocalDate.of(2023, 4, 3))
-                .assetId(111L)
-                .settlementInvoiceAmount(BigDecimal.valueOf(234.03))
-                .paidState("paid")
-                .invoiceType("reservation_statement")
-                .build();
-        return List.of(invoice1, invoice2);
-    }
-
-    private List<FinanceInvoice> getTestFinanceInvoices() {
-        FinanceInvoice financeInvoice1 =  FinanceInvoice.builder()
-                .invoiceId(20230103L)
-                .invoiceDate(LocalDate.of(2023, 1, 3))
-                .hotelId(111L)
-                .commission(BigDecimal.valueOf(231.03))
-                .paidState("paid")
-                .type("reservation_statement")
-                .build();
-        FinanceInvoice financeInvoice2 =  FinanceInvoice.builder()
-                .invoiceId(20230203L)
-                .invoiceDate(LocalDate.of(2023, 2, 3))
-                .hotelId(111L)
-                .commission(BigDecimal.valueOf(232.03))
-                .paidState("unpaid")
-                .type("reservation_statement")
-                .build();
-        return List.of(financeInvoice1, financeInvoice2);
-    }
-
-    private Debtor getTestDebtor() {
-        return Debtor.builder()
-                .debtorId(111L)
-                .contractedBy("NON-BRAZIL")
-                .build();
-    }
-
-    private Debtor getBrazilDebtor() {
-        return Debtor.builder()
-                .debtorId(BRAZIL_DEBTOR_ID)
-                .contractedBy("CONTRACTED_BY_BRAZIL")
-                .build();
-    }
-
-    private List<InvoiceData> getTestInvoices() {
+    private List<InvoiceData> getExpectedNonBrazilInvoices() {
         InvoiceData invoiceData1 = new InvoiceData();
-        invoiceData1.setAssetId(111L);
+        invoiceData1.setAssetId(DebtorFactory.getNonBrazilDebtorId());
         invoiceData1.setExternalId("20230103");
         invoiceData1.setInvoiceType("invoice");
         invoiceData1.setCommissionAmount(BigDecimal.valueOf(231.03));
         invoiceData1.setInvoiceType("reservation_statement");
         invoiceData1.setInvoiceDate(LocalDate.of(2023, 1, 3));
+        invoiceData1.setAdyenAllowed(true);
+        invoiceData1.setCompany(FinConstants.COMPANY_BOOKING_BV);
         InvoiceData invoiceData2 = new InvoiceData();
-        invoiceData2.setAssetId(111L);
+        invoiceData2.setAssetId(DebtorFactory.getNonBrazilDebtorId());
         invoiceData2.setExternalId("20230203");
         invoiceData2.setInvoiceType("invoice");
         invoiceData2.setCommissionAmount(BigDecimal.valueOf(232.03));
         invoiceData2.setInvoiceType("reservation_statement");
         invoiceData2.setInvoiceDate(LocalDate.of(2023, 2, 3));
+        invoiceData2.setAdyenAllowed(true);
+        invoiceData2.setCompany(FinConstants.COMPANY_BOOKING_BV);
         InvoiceData invoiceData3 = new InvoiceData();
-        invoiceData3.setAssetId(111L);
+        invoiceData3.setAssetId(DebtorFactory.getNonBrazilDebtorId());
         invoiceData3.setExternalId("20230303");
         invoiceData3.setInvoiceType("invoice");
         invoiceData3.setCommissionAmount(BigDecimal.valueOf(233.03));
         invoiceData3.setInvoiceType("reservation_statement");
         invoiceData3.setInvoiceDate(LocalDate.of(2023, 3, 3));
+        invoiceData3.setAdyenAllowed(true);
+        invoiceData3.setCompany(FinConstants.COMPANY_BOOKING_BV);
         InvoiceData invoiceData4 = new InvoiceData();
-        invoiceData4.setAssetId(111L);
+        invoiceData4.setAssetId(DebtorFactory.getNonBrazilDebtorId());
         invoiceData4.setExternalId("20230403");
         invoiceData4.setInvoiceType("invoice");
         invoiceData4.setCommissionAmount(BigDecimal.valueOf(234.03));
         invoiceData4.setInvoiceType("reservation_statement");
         invoiceData4.setInvoiceDate(LocalDate.of(2023, 4, 3));
+        invoiceData4.setAdyenAllowed(true);
+        invoiceData4.setCompany(FinConstants.COMPANY_BOOKING_BV);
         /*
         InvoiceData invoiceData1 = InvoiceData.builder()
-                .assetId(111L)
+                .assetId(DebtorFactory.getNonBrazilDebtorId())
                 .externalId("20230103")
                 .invoiceType("invoice")
                 .commissionAmount(BigDecimal.valueOf(231.03))
                 .invoiceType("reservation_statement")
                 .invoiceDate(LocalDate.of(2023, 1, 3))
+                .adyenAllowed(true)
+                .company(FinConstants.COMPANY_BOOKING_BV)
                 .build();
         InvoiceData invoiceData2 = InvoiceData.builder()
-                .assetId(111L)
+                .assetId(DebtorFactory.getNonBrazilDebtorId())
                 .externalId("20230203")
                 .invoiceType("invoice")
                 .commissionAmount(BigDecimal.valueOf(232.03))
                 .invoiceType("reservation_statement")
                 .invoiceDate(LocalDate.of(2023, 2, 3))
+                .adyenAllowed(true)
+                .company(FinConstants.COMPANY_BOOKING_BV)
                 .build();
         InvoiceData invoiceData3 = InvoiceData.builder()
-                .assetId(111L)
+                .assetId(DebtorFactory.getNonBrazilDebtorId())
                 .externalId("20230303")
                 .invoiceType("invoice")
                 .commissionAmount(BigDecimal.valueOf(233.03))
                 .invoiceType("reservation_statement")
                 .invoiceDate(LocalDate.of(2023, 3, 3))
+                .adyenAllowed(true)
+                .company(FinConstants.COMPANY_BOOKING_BV)
                 .build();
         InvoiceData invoiceData4 = InvoiceData.builder()
-                .assetId(111L)
+                .assetId(DebtorFactory.getNonBrazilDebtorId())
                 .externalId("20230403")
                 .invoiceType("invoice")
                 .commissionAmount(BigDecimal.valueOf(234.03))
                 .invoiceType("reservation_statement")
                 .invoiceDate(LocalDate.of(2023, 4, 3))
+                .adyenAllowed(true)
+                .company(FinConstants.COMPANY_BOOKING_BV)
                 .build();
          */
         return List.of(invoiceData1, invoiceData2, invoiceData3, invoiceData4);
     }
 
-    private List<InvoiceData> getTestBrazilInvoices() {
+    private List<InvoiceData> getExpectedBrazilInvoices() {
         InvoiceData invoiceData1 = new InvoiceData();
-        invoiceData1.setAssetId(BRAZIL_DEBTOR_ID);
+        invoiceData1.setAssetId(DebtorFactory.getBrazilDebtorId());
         invoiceData1.setExternalId("20220103");
         invoiceData1.setInvoiceType("invoice");
         invoiceData1.setCommissionAmount(BigDecimal.valueOf(221.03));
@@ -253,8 +161,10 @@ class InvoiceListerServiceTest {
         invoiceData1.setInvoiceDate(LocalDate.of(2022, 1, 3));
         invoiceData1.setRpsNumber(9999L);
         invoiceData1.setPrefeituraUrl("http://www.prefeitura.com/20220103");
+        invoiceData1.setAdyenAllowed(false);
+        invoiceData1.setCompany(FinConstants.COMPANY_BOOKING_LTDA);
         InvoiceData invoiceData2 = new InvoiceData();
-        invoiceData2.setAssetId(BRAZIL_DEBTOR_ID);
+        invoiceData2.setAssetId(DebtorFactory.getBrazilDebtorId());
         invoiceData2.setExternalId("20220203");
         invoiceData2.setInvoiceType("invoice");
         invoiceData2.setCommissionAmount(BigDecimal.valueOf(222.03));
@@ -262,9 +172,11 @@ class InvoiceListerServiceTest {
         invoiceData2.setInvoiceDate(LocalDate.of(2022, 2, 3));
         invoiceData2.setRpsNumber(8888L);
         invoiceData2.setPrefeituraUrl("http://www.prefeitura.com/20220203");
+        invoiceData2.setAdyenAllowed(false);
+        invoiceData2.setCompany(FinConstants.COMPANY_BOOKING_LTDA);
         /*
         InvoiceData invoiceData1 = InvoiceData.builder()
-                .assetId(BRAZIL_DEBTOR_ID)
+                .assetId(DebtorFactory.getBrazilDebtorId())
                 .externalId("20220103")
                 .invoiceType("invoice")
                 .commissionAmount(BigDecimal.valueOf(221.03))
@@ -272,9 +184,11 @@ class InvoiceListerServiceTest {
                 .invoiceDate(LocalDate.of(2022, 1, 3))
                 .rpsNumber(9999L)
                 .prefeituraUrl("http://www.prefeitura.com/20220103")
+                .adyenAllowed(false)
+                .company(FinConstants.COMPANY_BOOKING_LTDA)
                 .build();
         InvoiceData invoiceData2 = InvoiceData.builder()
-                .assetId(BRAZIL_DEBTOR_ID)
+                .assetId(DebtorFactory.getBrazilDebtorId())
                 .externalId("20220203")
                 .invoiceType("invoice")
                 .commissionAmount(BigDecimal.valueOf(222.03))
@@ -282,33 +196,10 @@ class InvoiceListerServiceTest {
                 .invoiceDate(LocalDate.of(2022, 2, 3))
                 .rpsNumber(8888L)
                 .prefeituraUrl("http://www.prefeitura.com/20220203")
+                .adyenAllowed(false)
+                .company(FinConstants.COMPANY_BOOKING_LTDA)
                 .build();*/
         return List.of(invoiceData1, invoiceData2);
     }
 
-    private List<SimpleInvoiceProjectionDTO> getNbeBrazilInvoices() {
-        SimpleInvoiceProjectionDTO invoice1 = SimpleInvoiceProjectionDTO.builder()
-                .externalId("20220103")
-                .invoiceDate(LocalDate.of(2022, 1, 3))
-                .assetId(BRAZIL_DEBTOR_ID)
-                .settlementInvoiceAmount(BigDecimal.valueOf(221.03))
-                .paidState("paid")
-                .invoiceType("reservation_statement")
-                .extraInfo_rpsNumber(9999L)
-                .extraInfo_prefeituraUrl("http://www.prefeitura.com/20220103")
-                .build();
-        return List.of(invoice1);
-    }
-
-    private List<FinanceInvoice> getTestFinanceBrazilInvoices() {
-        FinanceInvoice financeInvoice1 =  FinanceInvoice.builder()
-                .invoiceId(20220203L)
-                .invoiceDate(LocalDate.of(2022, 2, 3))
-                .hotelId(BRAZIL_DEBTOR_ID)
-                .commission(BigDecimal.valueOf(222.03))
-                .paidState("paid")
-                .type("reservation_statement")
-                .build();
-        return List.of(financeInvoice1);
-    }
 }
